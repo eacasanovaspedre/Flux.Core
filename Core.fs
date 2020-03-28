3f383faea41dd0c940b004faf7eed2ddc65bc79b
Enrique Alfonso Casanovas Pedre <eacasanovaspedre@outlook.com>
2020-03-28 15:33:28 -0300

    First functionality

---
+++ Core.fs
@@ -0,0 +1,94 @@
+﻿namespace Flux
+
+open System.Text.RegularExpressions
+
+[<Struct>]
+type NEString = private | NEString of string
+
+type CannotCreateNEString = CannotCreateNEString
+
+exception CannotCreateNEStringException
+
+module NEString =
+
+    let tryCreate value =
+        if String.length value > 0 then Ok(NEString value) else Error CannotCreateNEString
+
+    let maybeCreate value =
+        if String.length value > 0 then Some(NEString value) else None
+
+    let create value =
+        if String.length value > 0 then NEString value else raise (CannotCreateNEStringException)
+
+    let stringValue (NEString value) = value
+
+[<Struct>]
+type Email = private | Email of value: string
+
+type InvalidEmailFormat = InvalidEmailFormat of string
+
+exception InvalidEmailFormatException of InvalidEmailFormat
+
+module Email =
+
+    let private regex =
+        Regex
+            (@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+             RegexOptions.Compiled ||| RegexOptions.IgnoreCase)
+
+    let tryCreate email =
+        let result = regex.Match email
+        if result.Success then Ok(Email email) else Error(InvalidEmailFormat email)
+
+    let maybeCreate email =
+        let result = regex.Match email
+        if result.Success then Some(Email email) else None
+
+    let create email =
+        let result = regex.Match email
+        if result.Success
+        then Email email
+        else raise (InvalidEmailFormatException(InvalidEmailFormat email))
+
+    let stringValue (Email email) = email
+
+module Text =
+    open System.Text
+
+    let bytesToStringUTF8 (bytes: byte []) = Encoding.UTF8.GetString bytes
+
+    let stringToBytesUTF8 (str: string) = Encoding.UTF8.GetBytes str
+
+module Result =
+    let safe f e x =
+        try
+            x
+            |> f
+            |> Ok
+        with ex ->
+            ex
+            |> e
+            |> Error
+
+    let iterEither f e x =
+        match x with
+        | Ok v -> f v
+        | Error er -> e er
+        x
+
+namespace Flux.LanguageUtils.Linq.Expressions
+
+open System.Linq.Expressions
+open System
+
+type 'T Pred = Expression<Func<'T, bool>>
+
+type Mapper<'T, 'R> = Expression<Func<'T, 'R>>
+
+type Selector<'T, 'R> = Mapper<'T, 'R>
+
+type Expr private () =
+    static member Of(e: Expression<Func<'T1, 'TResult>>) = e
+    static member Of(e: Expression<Func<'T1, 'T2, 'TResult>>) = e
+    static member Of(e: Expression<Func<'T1, 'T2, 'T3, 'TResult>>) = e
+    static member Of(e: Expression<Func<'T1, 'T2, 'T3, 'T4, 'TResult>>) = e
---
+++ DotnetSupport.fs
@@ -0,0 +1,51 @@
+[<AutoOpen>]
+module Flux.DotNetSupport
+open System
+
+let fromFunc0 (f: Func<_>) = fun () -> f.Invoke()
+
+let fromFunc1 (f: Func<_, _>) = f.Invoke
+
+let fromFunc2 (f: Func<_, _, _>) = fun a b -> f.Invoke (a, b)
+
+let fromFunc3 (f: Func<_, _, _, _>) = fun a b c -> f.Invoke (a, b, c)
+
+let fromFunc4 (f: Func<_, _, _, _, _>) = fun a b c d -> f.Invoke (a, b, c, d)
+
+let fromFunc5 (f: Func<_, _, _, _, _, _>) = fun a b c d e -> f.Invoke (a, b, c, d, e)
+
+let fromAction (f: Action) = f.Invoke
+
+let fromAction1 (f: Action<_>) = f.Invoke
+
+let fromAction2 (f: Action<_, _>) = fun a b -> f.Invoke (a, b)
+
+let fromAction3 (f: Action<_, _, _>) = fun a b c -> f.Invoke (a, b, c)
+
+let fromAction4 (f: Action<_, _, _, _>) = fun a b c d -> f.Invoke (a, b, c, d)
+
+let fromAction5 (f: Action<_, _, _, _, _>) = fun a b c d e -> f.Invoke (a, b, c, d, e)
+
+let (|Func|) f = fromFunc0 f
+
+let (|Func1|) f = fromFunc1 f
+
+let (|Func2|) f = fromFunc2 f
+
+let (|Func3|) f = fromFunc3 f
+
+let (|Func4|) f = fromFunc4 f
+
+let (|Func5|) f = fromFunc5 f
+
+let (|Action|) f = fromAction f
+
+let (|Action1|) f = fromAction1 f
+
+let (|Action2|) f = fromAction2 f
+
+let (|Action3|) f = fromAction3 f
+
+let (|Action4|) f = fromAction4 f
+
+let (|Action5|) f = fromAction5 f
